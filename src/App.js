45fbf961699982eea3f4295062ad9931067aae89
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-12 14:57:14 -0300

    Create function to compare columns

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.textAreaRef = React.createRef();
-    this.selectedFile = React.createRef();
+    this.selectedFile1 = React.createRef();
+    this.selectedFile2 = React.createRef();
+    this.state = {table1: null, table2: null};
   }
 
   render() {
@@ -13,16 +15,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <div className="input-file">
-          <label className="btn" htmlFor="json-file">Choose json file</label>
+          <label className="btn" htmlFor="json-file">Choose paremeter json file</label>
           <input id="json-file" type="file" onChange={this.onInputJSONChange} accept=".json"/>
         </div>
         <div className="input-file">
           <label className="btn" htmlFor="csv-file">Choose csv file</label>
           <input id="csv-file" type="file" onChange={this.onInputCSVChange} accept=".csv" />
         </div>
-        <p ref={this.selectedFile}></p>
+        <p ref={this.selectedFile1}></p>
+        <div className="input-file">
+          <label className="btn" htmlFor="csv-file2">Choose csv2 file</label>
+          <input id="csv-file2" type="file" onChange={this.onInputCSVChange2} accept=".csv" />
+        </div>
+        <p ref={this.selectedFile2}></p>
         </header>
         <article>
+          <button className="btn" onClick={this.compareColumns}>Compare columns</button>
           <textarea className="" rows="25" cols="80" ref={this.textAreaRef}></textarea>
         </article>
       </div>
@@ -36,17 +44,29 @@ class App extends Component {
     reader.onload = event => {
       const CSV = event.target.result; 
       const arrayCSV = this.convertCSVToJSON(CSV);
-      this.textAreaRef.current.value = JSON.stringify(arrayCSV);
+      this.setState({table1: arrayCSV});
+      this.selectedFile1.current.innerHTML = file.name;
     }
-    this.selectedFile.current.innerHTML = file.name;
   }
 
+  onInputCSVChange2 = event => {
+    const reader = new FileReader();
+    const file = event.target.files[0];
+    reader.readAsText(file);
+    reader.onload = event => {
+      const CSV = event.target.result; 
+      const arrayCSV = this.convertCSVToJSON(CSV);
+      this.setState({table2: arrayCSV});
+      this.selectedFile2.current.innerHTML = file.name;
+    }
+  }
+  
   convertCSVToJSON(CSVContent){
     const splitedBrokenLine = CSVContent.split('\n');
     const sizeWithOnlyCommas = 12;
     const columnsName = splitedBrokenLine[0].split(',');
     const rows = splitedBrokenLine.filter((row, index) => 
-      index >= 1 && row.length > sizeWithOnlyCommas);
+    index >= 1 && row.length > sizeWithOnlyCommas);
     let clientList = {};
     let clients = [];
     for(let i = 1; i < rows.length; i++){
@@ -59,7 +79,7 @@ class App extends Component {
     }
     return clients;
   }
-
+  
   onInputJSONChange = event => {
     const reader = new FileReader();
     const file = event.target.files[0];
@@ -70,12 +90,43 @@ class App extends Component {
     reader.readAsText(file);
     this.selectedFile.current.innerHTML = file.name;
   }
-
-  compareColumns(obj){
-    obj.forEach(json => {
-      console.log(json.from.column);
-      console.log(json.to.column);
-    });
+  
+  compareColumns = () => {
+    const {table1, table2} = this.state;
+    if(table1 === null){
+      alert("Select first file");
+    }
+    if(table2 === null){
+      alert("Select second file");
+    }
+    if(!(table1 === null) && !(table2 === null)){
+      const amountOfRows = table1.length;
+      let amountOfDiscrepancys = 0;
+      for(let i = 0; i < amountOfRows; i++){
+        const tb1 = table1[i];
+        const tb2 = table2[i];
+        const jsonObj1 = Object.keys(tb1);
+        const jsonObj2 = Object.keys(tb2);
+        const amountOfCells = jsonObj1.length;
+        for(let j = 0; j < amountOfCells; j++){
+          const key1 = jsonObj1[j];
+          const key2 = jsonObj2[j];
+          const value1 = tb1[key1];
+          const value2 = tb2[key2];
+          const hasDiscrepancys =  value1 !== value2;
+          if(hasDiscrepancys){
+            this.textAreaRef.current.value += `
+              Table1[${key1}]: ${value1}
+              Table2[${key2}]: ${value2} 
+              Has discrepancys: ${hasDiscrepancys ? "YES" : "NO"}`;
+            amountOfDiscrepancys++;
+          }
+        }
+      }
+      if(amountOfDiscrepancys === 0){
+        this.textAreaRef.current.value = `The tables don't have discrepancys`;
+      }
+    }
   }
 }
 
